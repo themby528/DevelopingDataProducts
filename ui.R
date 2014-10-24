163d8b48632ad6d2831fe6e3eb6b57cf54ea64b2
themby528 <taylor.arrowood@gmail.com>
2014-10-24 08:06:20 -0400

    Second Commit
    Handle Errors

--- ui.R
+++ ui.R
@@ -1,6 +1,11 @@
 #Code for Course Project ShinyApp
 #Calculate the user's current BMI and provide suggested BMI and tips to get there
 
+#setwd("~/Data Scientist Spec/Developing Data Products/Course Project")
+#library(shiny)
+#shinyapps::setAccountInfo(name='themby528',token='AB9F12D3BB17E22AED5A685993431D38',secret='M+CWBIX6zChknmXwGfU5mPxWhb0jd2kF6D1FOqtS')
+#library(shinyapps)
+
 library(shiny)
 
 shinyUI(pageWithSidebar(
@@ -15,8 +20,8 @@ shinyUI(pageWithSidebar(
         numericInput("userWeight", label = h5("Please enter your current weight in pounds"),value=""),
         h5("Please enter your current height in a combination of feet and inches."),
         div(class="row-fluid",
-            div(class="span1",numericInput("userHeightFeet", label = "feet", value="")),
-            div(class="span1",numericInput("userHeightInches", label = "inches", value=""))),
+            div(class="span6",numericInput("userHeightFeet", label = "feet", value="",min=0,max=12,step=1)),
+            div(class="span6",numericInput("userHeightInches", label = "inches", value="",min=0,max=11,step=1))),
         radioButtons("userSex",label = h5("Please select your sex"), choices = list("Female"=1,"Male"=2)),
         numericInput("userWaist", label = h5("Please enter your waist size in inches"), value="")
         ),
